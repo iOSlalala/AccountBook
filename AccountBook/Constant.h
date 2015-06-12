730bb644bdbd2b605f4b4992fead80db6fd3a10f
wangxinyan <wangxinyan@wangxinyans-MacBook-Pro.local>
2015-06-12 14:22:19 +0800

    constant

---
+++ AccountBook/Constant.h
@@ -0,0 +1,15 @@
+//
+//  Constant.h
+//  AccountBook
+//
+//  Created by 王馨妍 on 15/6/12.
+//  Copyright (c) 2015年 wxy. All rights reserved.
+//
+
+#ifndef AccountBook_Constant_h
+#define AccountBook_Constant_h
+
+#define SCREEN_WIDTH  [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+
+#endif
